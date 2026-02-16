6bf0b5e7c4c6afe67f6dc77fd4007fe98b7e62df
gustcustodio <gustcustodio@gmail.com>
2026-02-16 13:47:28 -0300

    create project

---
+++ src/main/java/com/gustcustodio/to_do_list_api/ToDoListApiApplication.java
@@ -0,0 +1,13 @@
+package com.gustcustodio.to_do_list_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustcustodio/to_do_list_api/ToDoListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustcustodio.to_do_list_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
