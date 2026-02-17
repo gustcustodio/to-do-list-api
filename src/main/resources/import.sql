7e1ec93685c8c935d85e4a97e663b9c607d2da0d
gustcustodio <gustcustodio@gmail.com>
2026-02-17 10:06:53 -0300

    add database seeding

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_item(title, description) VALUES ('Buy groceries', 'Buy milk, eggs, and bread')
+INSERT INTO tb_item(title, description) VALUES ('Pay bills', 'Pay electricity and water bills')
+INSERT INTO tb_item(title, description) VALUES ('Study Spring Security', 'Read documentation about JWT and FilterChain');
+INSERT INTO tb_item(title, description) VALUES ('Gym session', 'Upper body workout and 20 minutes of cardio');
+INSERT INTO tb_item(title, description) VALUES ('Clean the house', 'Vacuum the living room and wash the dishes');
