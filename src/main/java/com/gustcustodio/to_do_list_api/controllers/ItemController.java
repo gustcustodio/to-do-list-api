6a6cc930037e1a20e34fdd4c674cd6a88f9254da
gustcustodio <gustcustodio@gmail.com>
2026-02-17 10:17:09 -0300

    add getSingleToDoItem

--- src/main/java/com/gustcustodio/to_do_list_api/controllers/ItemController.java
+++ src/main/java/com/gustcustodio/to_do_list_api/controllers/ItemController.java
@@ -1,6 +1,10 @@
 package com.gustcustodio.to_do_list_api.controllers;
 
+import com.gustcustodio.to_do_list_api.dtos.ItemDTO;
 import com.gustcustodio.to_do_list_api.services.ItemService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +16,12 @@ public class ItemController {
 
     public ItemController(ItemService itemService) {
         this.itemService = itemService;
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ItemDTO> getSingleToDoItem(@PathVariable Long id) {
+        ItemDTO itemDTO = itemService.getSingleToDoItem(id);
+        return ResponseEntity.ok(itemDTO);
     }
 
 }
--- src/main/java/com/gustcustodio/to_do_list_api/services/ItemService.java
+++ src/main/java/com/gustcustodio/to_do_list_api/services/ItemService.java
@@ -1,16 +1,27 @@
 package com.gustcustodio.to_do_list_api.services;
 
 import com.gustcustodio.to_do_list_api.dtos.ItemDTO;
+import com.gustcustodio.to_do_list_api.entities.Item;
+import com.gustcustodio.to_do_list_api.mappers.ItemMapper;
 import com.gustcustodio.to_do_list_api.repositories.ItemRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ItemService {
 
     private final ItemRepository itemRepository;
+    private final ItemMapper itemMapper;
 
-    public ItemService(ItemRepository itemRepository) {
+    public ItemService(ItemRepository itemRepository, ItemMapper itemMapper) {
         this.itemRepository = itemRepository;
+        this.itemMapper = itemMapper;
+    }
+
+    @Transactional(readOnly = true)
+    public ItemDTO getSingleToDoItem(Long id) {
+        Item item = itemRepository.findById(id).orElseThrow();
+        return itemMapper.convertEntityToDto(item);
     }
 
 }
