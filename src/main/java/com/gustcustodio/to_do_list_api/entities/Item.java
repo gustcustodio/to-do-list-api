e834062d0e3152076d4a70f9407beed0bb55e2c0
gustcustodio <gustcustodio@gmail.com>
2026-02-17 10:05:41 -0300

    create Item

---
+++ src/main/java/com/gustcustodio/to_do_list_api/entities/Item.java
@@ -0,0 +1,31 @@
+package com.gustcustodio.to_do_list_api.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@Entity
+@Table(name = "tb_item")
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+}
