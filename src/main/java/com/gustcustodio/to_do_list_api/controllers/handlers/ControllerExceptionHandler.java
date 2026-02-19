5a755a84856012f3a641e73de7389209f1d8046a
gustcustodio <gustcustodio@gmail.com>
2026-02-19 07:02:01 -0300

    add exception handler to validations

--- src/main/java/com/gustcustodio/to_do_list_api/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/gustcustodio/to_do_list_api/controllers/handlers/ControllerExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.gustcustodio.to_do_list_api.controllers.handlers;
 
 import com.gustcustodio.to_do_list_api.dtos.CustomErrorDTO;
+import com.gustcustodio.to_do_list_api.dtos.ValidationErrorDTO;
 import com.gustcustodio.to_do_list_api.services.exceptions.DatabaseException;
 import com.gustcustodio.to_do_list_api.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -26,6 +29,16 @@ public class ControllerExceptionHandler {
         HttpStatus status = HttpStatus.BAD_REQUEST;
         CustomErrorDTO customErrorDTO = new CustomErrorDTO(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(customErrorDTO);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomErrorDTO> methodArgumentNotValid(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationErrorDTO validationErrorDTO = new ValidationErrorDTO(Instant.now(), status.value(), "Dados inválidos", request.getRequestURI());
+        for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+            validationErrorDTO.addError(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return ResponseEntity.status(status).body(validationErrorDTO);
     }
 
 }
