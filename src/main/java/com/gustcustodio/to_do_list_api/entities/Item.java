6e7148a1b735edfade371824c329526e2cf24074
gustcustodio <gustcustodio@gmail.com>
2026-02-20 07:05:08 -0300

    add mapping

--- src/main/java/com/gustcustodio/to_do_list_api/entities/Item.java
+++ src/main/java/com/gustcustodio/to_do_list_api/entities/Item.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -27,5 +29,9 @@ public class Item {
     private String title;
 
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
 }
