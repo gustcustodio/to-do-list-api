6583165a878decce8642402e17772ba694b1cd44
gustcustodio <gustcustodio@gmail.com>
2026-02-20 07:04:18 -0300

    create User

---
+++ src/main/java/com/gustcustodio/to_do_list_api/entities/User.java
@@ -0,0 +1,36 @@
+package com.gustcustodio.to_do_list_api.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @OneToMany(mappedBy = "user")
+    private final List<Item> items = new ArrayList<>();
+
+}
