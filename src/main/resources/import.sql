37d00699fc79cfdfc3d4c01fb87fdf279afb30d7
gustcustodio <gustcustodio@gmail.com>
2026-02-20 07:05:51 -0300

    change database seeding

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,7 @@
-INSERT INTO tb_item(title, description) VALUES ('Buy groceries', 'Buy milk, eggs, and bread')
-INSERT INTO tb_item(title, description) VALUES ('Pay bills', 'Pay electricity and water bills')
-INSERT INTO tb_item(title, description) VALUES ('Study Spring Security', 'Read documentation about JWT and FilterChain');
-INSERT INTO tb_item(title, description) VALUES ('Gym session', 'Upper body workout and 20 minutes of cardio');
-INSERT INTO tb_item(title, description) VALUES ('Clean the house', 'Vacuum the living room and wash the dishes');
+INSERT INTO tb_user(name, email, password) VALUES ('Gon Freecss', 'gon@email.com', '123456')
+INSERT INTO tb_user(name, email, password) VALUES ('Killua Zoldyck ', 'kilua@email.com', '123456')
+
+INSERT INTO tb_item(title, description, user_id) VALUES ('Buy groceries', 'Buy milk, eggs, and bread', 1)
+INSERT INTO tb_item(title, description, user_id) VALUES ('Pay bills', 'Pay electricity and water bills', 1)
+INSERT INTO tb_item(title, description, user_id) VALUES ('Gym session', 'Upper body workout and 20 minutes of cardio', 2);
+INSERT INTO tb_item(title, description, user_id) VALUES ('Clean the house', 'Vacuum the living room and wash the dishes', 2);
