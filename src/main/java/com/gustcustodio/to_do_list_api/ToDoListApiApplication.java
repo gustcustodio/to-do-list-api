584e841f6a38c7223623c3eca982f7a3bea17a79
gustcustodio <gustcustodio@gmail.com>
2026-02-22 14:19:07 -0300

    test passwordEncoder

--- src/main/java/com/gustcustodio/to_do_list_api/ToDoListApiApplication.java
+++ src/main/java/com/gustcustodio/to_do_list_api/ToDoListApiApplication.java
@@ -1,13 +1,26 @@
 package com.gustcustodio.to_do_list_api;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class ToDoListApiApplication {
+public class ToDoListApiApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ToDoListApiApplication.class, args);
-	}
+    private final PasswordEncoder passwordEncoder;
+
+    public ToDoListApiApplication(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoListApiApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        //		System.out.println("ENCODE = " + passwordEncoder.encode("123456"));
+    }
 
 }
