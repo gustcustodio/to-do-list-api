674c152d9a01007e4e1e4f655c8077212dce685d
gustcustodio <gustcustodio@gmail.com>
2026-02-22 14:20:04 -0300

    add register and login

--- src/main/java/com/gustcustodio/to_do_list_api/controllers/UserController.java
+++ src/main/java/com/gustcustodio/to_do_list_api/controllers/UserController.java
@@ -1,17 +1,36 @@
 package com.gustcustodio.to_do_list_api.controllers;
 
+import com.gustcustodio.to_do_list_api.dtos.LoginRequestDTO;
+import com.gustcustodio.to_do_list_api.dtos.LoginResponseDTO;
+import com.gustcustodio.to_do_list_api.dtos.RegisterDTO;
 import com.gustcustodio.to_do_list_api.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/user")
+@RequestMapping(value = "/todos")
 public class UserController {
 
     private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<LoginResponseDTO> register(@Valid @RequestBody RegisterDTO registerDTO) {
+        LoginResponseDTO loginResponseDTO = userService.register(registerDTO);
+        return ResponseEntity.ok(loginResponseDTO);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@Valid @RequestBody LoginRequestDTO loginRequestDTO) {
+        LoginResponseDTO loginResponseDTO = userService.login(loginRequestDTO);
+        return ResponseEntity.ok(loginResponseDTO);
     }
 
 }
--- src/main/java/com/gustcustodio/to_do_list_api/services/UserService.java
+++ src/main/java/com/gustcustodio/to_do_list_api/services/UserService.java
@@ -1,15 +1,56 @@
 package com.gustcustodio.to_do_list_api.services;
 
+import com.gustcustodio.to_do_list_api.dtos.LoginRequestDTO;
+import com.gustcustodio.to_do_list_api.dtos.LoginResponseDTO;
+import com.gustcustodio.to_do_list_api.dtos.RegisterDTO;
+import com.gustcustodio.to_do_list_api.entities.User;
 import com.gustcustodio.to_do_list_api.repositories.UserRepository;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
+    private final TokenService tokenService;
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(TokenService tokenService, UserRepository userRepository, PasswordEncoder passwordEncoder, @Lazy AuthenticationManager authenticationManager) {
+        this.tokenService = tokenService;
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+    }
+
+    public LoginResponseDTO register(RegisterDTO registerDTO) {
+        if (userRepository.findByEmail(registerDTO.email()) != null) throw new RuntimeException();
+
+        String encryptedPassword = passwordEncoder.encode(registerDTO.password());
+        User newUser = new User(registerDTO.name(), registerDTO.email(), encryptedPassword);
+        newUser = userRepository.save(newUser);
+
+        LoginResponseDTO loginResponseDTO = login(new LoginRequestDTO(newUser.getEmail(), registerDTO.password()));
+
+        return loginResponseDTO;
+    }
+
+    public LoginResponseDTO login(LoginRequestDTO loginRequestDTO) {
+        var user = new UsernamePasswordAuthenticationToken(loginRequestDTO.email(), loginRequestDTO.password());
+        var auth = authenticationManager.authenticate(user);
+        var token = tokenService.generateToken((User) auth.getPrincipal());
+        return new LoginResponseDTO(token);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username);
     }
 
 }
