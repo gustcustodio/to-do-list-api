8b64f8cf2b619c90baaa076b1db6f08ed360065c
gustcustodio <gustcustodio@gmail.com>
2026-02-22 14:18:08 -0300

    create AuthConfig

---
+++ src/main/java/com/gustcustodio/to_do_list_api/configs/AuthConfig.java
@@ -0,0 +1,23 @@
+package com.gustcustodio.to_do_list_api.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class AuthConfig {
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
