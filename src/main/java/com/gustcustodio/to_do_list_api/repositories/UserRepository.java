b8d2a52b1298fc9b00bae6e1b9395f8ba705b86e
gustcustodio <gustcustodio@gmail.com>
2026-02-22 14:13:13 -0300

    add query findByEmail

--- src/main/java/com/gustcustodio/to_do_list_api/repositories/UserRepository.java
+++ src/main/java/com/gustcustodio/to_do_list_api/repositories/UserRepository.java
@@ -2,8 +2,12 @@ package com.gustcustodio.to_do_list_api.repositories;
 
 import com.gustcustodio.to_do_list_api.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, String> {
+
+    UserDetails findByEmail(String email);
+
 }
