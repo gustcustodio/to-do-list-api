c9f6a7e257c16cfc406fdec6d1638bca432d1170
gustcustodio <gustcustodio@gmail.com>
2026-02-22 14:12:52 -0300

    add UserRole and UserDetails

--- src/main/java/com/gustcustodio/to_do_list_api/entities/User.java
+++ src/main/java/com/gustcustodio/to_do_list_api/entities/User.java
@@ -1,23 +1,35 @@
 package com.gustcustodio.to_do_list_api.entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
-@EqualsAndHashCode
+@EqualsAndHashCode(of = "id")
 @Entity
 @Table(name = "tb_user")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,7 +42,26 @@ public class User {
 
     private String password;
 
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole = UserRole.USER;
+
     @OneToMany(mappedBy = "user")
     private final List<Item> items = new ArrayList<>();
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + this.userRole.getRole()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
 
 }
