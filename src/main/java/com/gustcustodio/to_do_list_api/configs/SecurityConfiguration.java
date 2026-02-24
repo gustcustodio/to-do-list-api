5c329d682babec5d0bc20645da0ca799d13be82f
gustcustodio <gustcustodio@gmail.com>
2026-02-24 06:55:56 -0300

    add exception handling (401 UNAUTHORIZED)

--- src/main/java/com/gustcustodio/to_do_list_api/configs/SecurityConfiguration.java
+++ src/main/java/com/gustcustodio/to_do_list_api/configs/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.gustcustodio.to_do_list_api.configs;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,15 +9,18 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
     private final SecurityFilter securityFilter;
+    private final HandlerExceptionResolver resolver;
 
-    public SecurityConfiguration(SecurityFilter securityFilter) {
+    public SecurityConfiguration(SecurityFilter securityFilter, @Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver) {
         this.securityFilter = securityFilter;
+        this.resolver = resolver;
     }
 
     @Bean
@@ -29,6 +33,11 @@ public class SecurityConfiguration {
                         .requestMatchers(HttpMethod.POST, "/todos/register").permitAll()
                         .requestMatchers("/h2-console/**").permitAll()
                         .anyRequest().authenticated()
+                )
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .authenticationEntryPoint((request, response, authException) -> {
+                            resolver.resolveException(request, response, null, authException);
+                        })
                 )
                 .headers(headers -> headers.frameOptions(f -> f.sameOrigin()))
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
